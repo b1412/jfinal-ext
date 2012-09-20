1a6780a18f9f6a81a22bc781ce08290d541de453
zhoulei <zhoulei@mobicloud.com.cn>
2012-09-20 16:21:17 +0800

    refactor tablebind plugin

--- src/com/jfinal/plugin/tablebind/AutoTableBindPlugin.java
+++ src/com/jfinal/plugin/tablebind/AutoTableBindPlugin.java
@@ -4,9 +4,6 @@ import java.util.List;
 
 import javax.sql.DataSource;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import com.jfinal.plugin.activerecord.ActiveRecordPlugin;
 import com.jfinal.plugin.activerecord.IDataSourceProvider;
 import com.jfinal.plugin.activerecord.Model;
@@ -14,8 +11,6 @@ import com.jfinal.util.StringKit;
 
 public class AutoTableBindPlugin extends ActiveRecordPlugin {
 	private TableNameStyle tableNameStyle;
-	private Logger logger = LoggerFactory.getLogger(getClass());
-
 	public AutoTableBindPlugin(DataSource dataSource) {
 		super(dataSource);
 	}
@@ -25,24 +20,25 @@ public class AutoTableBindPlugin extends ActiveRecordPlugin {
 		this.tableNameStyle = tableNameStyle;
 	}
 
+	@SuppressWarnings("unchecked")
 	@Override
 	public boolean start() {
 		try {
 			List<Class> modelClasses = ClassSearcher.findClasses(Model.class);
-			logger.debug("modelClasses.size  {}",modelClasses.size());
+			System.out.println("modelClasses.size "+modelClasses.size());
 			TableBind tb = null;
 			for (Class modelClass : modelClasses) {
 				tb = (TableBind) modelClass.getAnnotation(TableBind.class);
 				if (tb == null) {
-					this.addMapping(tableName(modelClass), modelClass);
-					logger.debug("auto bindTable: addMapping({}, {})", tableName(modelClass), modelClass.getName());
+					this.addMapping(tableNameStyle.tableName(modelClass.getSimpleName()), modelClass);
+					System.out.println("auto bindTable: addMapping("+tableNameStyle.tableName(modelClass.getSimpleName())+", "+modelClass.getName()+")");
 				} else {
 					if (StringKit.notBlank(tb.pkName())) {
 						this.addMapping(tb.tableName(), tb.pkName(), modelClass);
-						logger.debug("auto bindTable: addMapping({}, {},{})", new Object[]{tb.tableName(),tb.pkName(), modelClass.getName()});
+						System.out.println("auto bindTable: addMapping("+tb.tableName()+", "+tb.pkName()+","+modelClass.getName()+")");
 					} else {
 						this.addMapping(tb.tableName(), modelClass);
-						logger.debug("auto bindTable: addMapping({}, {})", tb.tableName(), modelClass.getName());
+						System.out.println("auto bindTable: addMapping("+tb.tableName()+", "+modelClass.getName()+")");
 					}
 				}
 			}
@@ -54,18 +50,6 @@ public class AutoTableBindPlugin extends ActiveRecordPlugin {
 
 	@Override
 	public boolean stop() {
-		return true;
-	}
-
-	private String tableName(Class<?> clazz) {
-		String tableName = clazz.getSimpleName();
-		if (tableNameStyle == TableNameStyle.UP) {
-			tableName = tableName.toUpperCase();
-		} else if (tableNameStyle == TableNameStyle.LOWER) {
-			tableName = tableName.toLowerCase();
-		} else {
-			tableName = StringKit.firstCharToLowerCase(tableName);
-		}
-		return tableName;
+		return super.stop();
 	}
 }
--- src/com/jfinal/plugin/tablebind/TableNameStyle.java
+++ src/com/jfinal/plugin/tablebind/TableNameStyle.java
@@ -1,5 +1,41 @@
 package com.jfinal.plugin.tablebind;
 
+import com.jfinal.util.StringKit;
+
 public enum TableNameStyle {
-	UP, LOWER, UP_UNDERLINE, LOWER_UNDERLINE
+	DEFAULT {
+		public String tableName(String className) {
+			return className;
+		}
+	},
+	FIRST_LOWER {
+		public String tableName(String className) {
+			return StringKit.firstCharToLowerCase(className);
+		}
+	},
+	UP {
+		public String tableName(String className) {
+			return className.toUpperCase();
+		}
+	},
+	LOWER {
+		public String tableName(String className) {
+			return className.toLowerCase();
+		}
+	},
+	UP_UNDERLINE {
+		public String tableName(String className) {
+			// TODO
+			throw new RuntimeException("not finished");
+		}
+	},
+	LOWER_UNDERLINE {
+		public String tableName(String className) {
+			// TODO
+			throw new RuntimeException("not finished");
+		}
+	};
+
+	public abstract String tableName(String className);
+
 }
