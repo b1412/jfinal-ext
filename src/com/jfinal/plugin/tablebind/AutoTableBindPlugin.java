a514601bd4834ae344a9dad053442d54c70aed1a
zhoulei <zhoulei@mobicloud.com.cn>
2012-09-29 15:12:19 +0800

    add a new construtor in AutoTableBindPlugin

--- src/com/jfinal/plugin/tablebind/AutoTableBindPlugin.java
+++ src/com/jfinal/plugin/tablebind/AutoTableBindPlugin.java
@@ -11,11 +11,17 @@ import com.jfinal.util.StringKit;
 
 public class AutoTableBindPlugin extends ActiveRecordPlugin {
 	private TableNameStyle tableNameStyle;
+
 	public AutoTableBindPlugin(DataSource dataSource) {
 		super(dataSource);
 	}
 
-	public AutoTableBindPlugin(IDataSourceProvider dataSourceProvider, TableNameStyle tableNameStyle) {
+	public AutoTableBindPlugin(IDataSourceProvider dataSourceProvider) {
+		this(dataSourceProvider, TableNameStyle.DEFAULT);
+	}
+
+	public AutoTableBindPlugin(IDataSourceProvider dataSourceProvider,
+			TableNameStyle tableNameStyle) {
 		super(dataSourceProvider);
 		this.tableNameStyle = tableNameStyle;
 	}
@@ -25,20 +31,28 @@ public class AutoTableBindPlugin extends ActiveRecordPlugin {
 	public boolean start() {
 		try {
 			List<Class> modelClasses = ClassSearcher.findClasses(Model.class);
-			System.out.println("modelClasses.size "+modelClasses.size());
+			System.out.println("modelClasses.size " + modelClasses.size());
 			TableBind tb = null;
 			for (Class modelClass : modelClasses) {
 				tb = (TableBind) modelClass.getAnnotation(TableBind.class);
 				if (tb == null) {
-					this.addMapping(tableNameStyle.tableName(modelClass.getSimpleName()), modelClass);
-					System.out.println("auto bindTable: addMapping("+tableNameStyle.tableName(modelClass.getSimpleName())+", "+modelClass.getName()+")");
+					this.addMapping(tableNameStyle.tableName(modelClass
+							.getSimpleName()), modelClass);
+					System.out.println("auto bindTable: addMapping("
+							+ tableNameStyle.tableName(modelClass
+									.getSimpleName()) + ", "
+							+ modelClass.getName() + ")");
 				} else {
 					if (StringKit.notBlank(tb.pkName())) {
 						this.addMapping(tb.tableName(), tb.pkName(), modelClass);
-						System.out.println("auto bindTable: addMapping("+tb.tableName()+", "+tb.pkName()+","+modelClass.getName()+")");
+						System.out.println("auto bindTable: addMapping("
+								+ tb.tableName() + ", " + tb.pkName() + ","
+								+ modelClass.getName() + ")");
 					} else {
 						this.addMapping(tb.tableName(), modelClass);
-						System.out.println("auto bindTable: addMapping("+tb.tableName()+", "+modelClass.getName()+")");
+						System.out.println("auto bindTable: addMapping("
+								+ tb.tableName() + ", " + modelClass.getName()
+								+ ")");
 					}
 				}
 			}
