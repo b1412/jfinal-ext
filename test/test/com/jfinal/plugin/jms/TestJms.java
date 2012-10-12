f9b11b4af125c7a71e01d37f68216b8a6204f292
Zhou Lei <zhoulei@mobicloud.com.cn>
2012-10-12 14:23:38 +0800

    add TestCase of ConfigPlugin support for JmsPlugin

--- test/test/com/jfinal/plugin/jms/TestJms.java
+++ test/test/com/jfinal/plugin/jms/TestJms.java
@@ -1,31 +1,29 @@
 package test.com.jfinal.plugin.jms;
 
-import java.io.Serializable;
 import java.util.concurrent.TimeUnit;
 
-import org.junit.BeforeClass;
 import org.junit.Test;
 
+import com.jfinal.plugin.config.ConfigPlugin;
 import com.jfinal.plugin.jms.JmsKit;
 import com.jfinal.plugin.jms.JmsPlugin;
 
 public class TestJms {
-	@BeforeClass
-	public static void init() {
-		JmsPlugin jmsPlugin = new JmsPlugin("test/com/jfinal/plugin/jms/jms.properties");
-		jmsPlugin.start();
-	}
-
 	@Test
 	public void sendQueue() throws InterruptedException {
+		JmsPlugin jmsPlugin = new JmsPlugin("jms.properties");
+		jmsPlugin.start();
 		JmsKit.sendQueue("q1", new M(), "a");
 		TimeUnit.SECONDS.sleep(60);
 	}
-
-}
-
-class M implements Serializable {
-
-	private static final long serialVersionUID = 1L;
+	@Test
+	public void sendQueue2() throws InterruptedException {
+		ConfigPlugin configPlugin = new ConfigPlugin("jms.properties");
+		JmsPlugin jmsPlugin = new JmsPlugin(configPlugin);
+		configPlugin.start();
+		jmsPlugin.start();
+		JmsKit.sendQueue("q1", new M(), "a");
+		TimeUnit.SECONDS.sleep(60);
+	}
 
 }
