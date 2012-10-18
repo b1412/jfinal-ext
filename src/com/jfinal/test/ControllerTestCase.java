260197411a79dcb22908fac52b7886a549ea5ea8
Zhou Lei <zhoulei@mobicloud.com.cn>
2012-10-18 13:11:11 +0800

    update ControllerTestCase to configure  JFinalConfig

--- src/com/jfinal/test/ControllerTestCase.java
+++ src/com/jfinal/test/ControllerTestCase.java
@@ -4,7 +4,6 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 import javax.servlet.ServletContext;
@@ -13,7 +12,6 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.junit.BeforeClass;
 
-import test.com.jfinal.plugin.ext.Config;
 
 import com.jfinal.config.JFinalConfig;
 import com.jfinal.core.JFinal;
@@ -24,11 +22,11 @@ public class ControllerTestCase {
 	protected static HttpServletRequest request = new MockHttpRequest();
 	protected static HttpServletResponse response = new MockHttpResponse();
 	protected static Handler handler ;
-	public static void start() throws Exception {
+	public static void start(JFinalConfig config) throws Exception {
 		Class<JFinal> clazz = JFinal.class;
 		JFinal me = JFinal.me();
 		when(servletContext.getRealPath("/")).thenReturn("/test");
-		initConfig(clazz, me, servletContext);
+		initConfig(clazz, me, servletContext,config);
 		Field field = me.getClass().getDeclaredField("handler");
 		field.setAccessible(true);
 		handler = (Handler) field.get(me);
@@ -42,16 +40,10 @@ public class ControllerTestCase {
 	}
 
 	private static void initConfig(Class<JFinal> clazz, JFinal me,
-			ServletContext servletContext) throws Exception {
-		JFinalConfig config = new Config();
+			ServletContext servletContext,JFinalConfig config) throws Exception {
 		Method method = clazz.getDeclaredMethod("init", JFinalConfig.class,
 				ServletContext.class);
 		method.setAccessible(true);
 		method.invoke(me, config, servletContext);
-	}
-	
-	@BeforeClass
-	public static void init() throws Exception{
-		start();
 	}
 }
--- test/test/com/jfinal/plugin/ext/TestRoute.java
+++ test/test/com/jfinal/plugin/ext/TestRoute.java
@@ -1,10 +1,15 @@
 package test.com.jfinal.plugin.ext;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import com.jfinal.test.ControllerTestCase;
 
 public class TestRoute extends ControllerTestCase {
+	@BeforeClass
+	public static void init() throws Exception{
+		start(new Config());
+	}
 	@Test
 	public void test() throws Exception {
 		invoke("/a");
