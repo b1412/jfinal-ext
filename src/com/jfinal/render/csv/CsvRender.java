a70f4174bf71ba058cf3697a8723b33c8692fe69
Zhou Lei <zhoulei@mobicloud.com.cn>
2012-10-23 17:55:22 +0800

    add csvUtil init cscRender

---
+++ src/com/jfinal/render/csv/CsvRender.java
@@ -0,0 +1,14 @@
+package com.jfinal.render.csv;
+
+import com.jfinal.render.Render;
+
+@SuppressWarnings("serial")
+public class CsvRender extends Render {
+
+	@Override
+	public void render() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
